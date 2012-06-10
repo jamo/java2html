af242f0c54b93c89563e8aac2593ccca031c6b74
Jarmo <jsi1992@gmail.com>
2012-06-11 01:33:07 +0300

    kirjoitus html filuun onnistuu

--- r.rb
+++ r.rb
@@ -4,7 +4,7 @@ require 'find'
 require 'cgi'
 include FileUtils
 include Find
- if !ARGV[0] || ['-h', '--help'].include?(ARGV[0])
+ if !ARGV[0] || !ARGV[1] || ['-h', '--help'].include?(ARGV[0])
    puts"Creates a new project from the project template."
    puts
    puts "Usage: #{__FILE__} [category/]project-name"
@@ -13,10 +13,12 @@ include Find
  end
 
 dest_path = ARGV[0]
+html_file_name = ARGV[1]
 
-
+@to_html = %Q{<!DOCTYPE HTML>\n<html>\n<meta charset="utd-8">\n<title>#{dest_path}</title>\n</head>\n<body>}
+@inside = ""
 def find_all_files dest_path
-  filetype = "erb"
+  filetype = "java"
   excludes = ["CVS","classes","images","lib","tlds"]
   files = Array.new
   Find.find(dest_path) do |path|
@@ -35,31 +37,38 @@ def find_all_files dest_path
   files
 end
 
-all_files = Array.new
-all_files += find_all_files dest_path
-
-
 def add_to_HTML strings
-  puts strings
+  @inside << %Q{<pre class = "sh_java">\n}
+  strings.each do |string|
+    @inside << string
+  end
+  @inside << %Q{</pre>\n}
 end
-
 
 def go_thru_files all_files
   strings = []
   for path in all_files
     File.open path,'r' do |f1|
       while line = f1.gets
-        #      do_something(line)
         strings << CGI::escapeHTML(line)
       end
       add_to_HTML strings
       strings = []
     end
   end
-
 end
 
+
+all_files = Array.new
+all_files += find_all_files dest_path
 go_thru_files all_files
 
 
+@to_html << @inside
+@to_html << %Q{</body>\n</html> }
 
+#puts @to_html
+
+File.open html_file_name,'w+' do |f|
+  f.puts @to_html
+end
